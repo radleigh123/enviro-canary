7925ba9bda9cfcdf0d4643f506c50fd53f03b925
Radleigh123 <103168649+radleigh123@users.noreply.github.com>
2025-05-12 15:12:33 +0800

    Home page FIX 2
    - remodeled bottom navigation
      - added icons 24x24
      - added icons 96x96
    - remodeled top app bar
    - Notification fragment (REMOVED) will now be activity

--- app/src/main/java/com/capstone/enviro/MainActivity.kt
+++ app/src/main/java/com/capstone/enviro/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.capstone.enviro
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstone.enviro.data.remote.RetrofitClient
 import com.capstone.enviro.data.remote.TokenManager
 import com.capstone.enviro.domain.model.User
 import com.capstone.enviro.domain.service.UserService
 import com.capstone.enviro.databinding.ActivityMainBinding
+import com.capstone.enviro.ui.home.NotificationActivity
+import com.capstone.enviro.ui.settings.SettingsActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,24 +36,68 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        setSupportActionBar(binding.topAppBar)
+        setSupportActionBar(binding.topAppBar)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_notification, R.id.navigation_profile
+                R.id.navigation_home,
+                R.id.navigation_map,
+                R.id.navigation_record,
+                R.id.navigation_activities,
+                R.id.navigation_you
             )
         )
 
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.navView.setupWithNavController(navController)
         // TODO: Later on, live data for notification count
-        updateNotificationBadge(5)
+//        updateNotificationBadge(5)
 
         sampleApiCall()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_notifications -> {
+                val intent = Intent(this, NotificationActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    private fun getSlideNavOptions(): NavOptions {
+        return NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_right)
+            .setExitAnim(R.anim.slide_out_left)
+            .setPopEnterAnim(R.anim.slide_in_left)
+            .setPopExitAnim(R.anim.slide_out_right)
+            .build()
     }
 
     private fun sampleApiCall() {
@@ -57,20 +112,20 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if (response.isSuccessful) {
                     val users = response.body()
-                    binding.testText.text = users.toString()
+                    Log.d("MainActivity", "Fetched users: $users")
                 }
             }
 
             override fun onFailure(call: Call<List<User>>, t: Throwable) {
                 Log.e("MainActivity", "Failed to fetch users", t)
-                binding.testText.text = "Failed to fetch users: ${t.message}"
             }
         })
     }
 
-    private fun updateNotificationBadge(count: Int) {
+    // TODO: Transfer badge update to top app bar
+    /*private fun updateNotificationBadge(count: Int) {
         val badge = binding.navView.getOrCreateBadge(R.id.navigation_notification)
         badge.isVisible = count > 0
         badge.number = count
-    }
+    }*/
 }
--- app/src/main/java/com/capstone/enviro/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/enviro/ui/home/HomeFragment.kt
@@ -13,10 +13,6 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.capstone.enviro.R
 import com.capstone.enviro.databinding.FragmentHomeBinding
 import com.capstone.enviro.ui.settings.SettingsActivity
@@ -36,30 +32,6 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        binding.topAppBar.title = "Home"
-
-        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.topAppBar)
-        requireActivity().addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.menu_main, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return when (menuItem.itemId) {
-                    R.id.action_settings -> {
-                        // Handle settings action
-                        Log.d("HomeFragment", "Settings clicked")
-
-                        val intent = Intent(requireContext(), SettingsActivity::class.java)
-                        startActivity(intent)
-
-                        true
-                    }
-                    else -> false
-                }
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         return binding.root
     }
